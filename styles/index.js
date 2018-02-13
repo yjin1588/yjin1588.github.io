b363d6edf1931f9c46f6a679b999b0241a9768e5
yjin1588 <yjin1588@gmail.com>
2018-02-13 15:45:34 +0900

    Add publications section

--- styles/index.js
+++ styles/index.js
@@ -1,11 +1,15 @@
 var hide_lists = function(cb) {
     $('#posts').fadeOut(300);
     $('#projects').fadeOut(300);
+    $('#publications').fadeOut(300);
     $('#posts-btn').removeClass('disabled');
-    $('#projects-btn').removeClass('disabled')
+    $('#projects-btn').removeClass('disabled');
+    $('#publications-btn').removeClass('disabled')
 };
 var show_projects = function() {
     $('#posts-btn').removeClass('disabled');
+    $('#publications-btn').removeClass('disabled');
+    $('#publications').fadeOut(300);
     $('#posts').fadeOut(300, function() {
         $('#projects').fadeIn(300)
     });
@@ -13,8 +17,19 @@ var show_projects = function() {
 };
 var show_posts = function() {
     $('#projects-btn').removeClass('disabled');
+    $('#publications-btn').removeClass('disabled');
+    $('#publications').fadeOut(300);
     $('#projects').fadeOut(function() {
         $('#posts').fadeIn(300)
     });
     $('#posts-btn').addClass('disabled')
+};
+var show_publications = function() {
+    $('#projects-btn').removeClass('disabled');
+    $('#posts-btn').removeClass('disabled');
+    $('#posts').fadeOut(300);
+    $('#projects').fadeOut(function() {
+        $('#publications').fadeIn(300)
+    });
+    $('#publications-btn').addClass('disabled')
 };
