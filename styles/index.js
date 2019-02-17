0b1672c6816e0cbffb70c9cefd6cc7db7fa923fe
yjin1588 <yjin1588@gmail.com>
2019-02-17 16:39:38 +0900

    add drawings layout

--- styles/index.js
+++ styles/index.js
@@ -3,33 +3,43 @@ var hide_lists = function(cb) {
     $('#projects').fadeOut(300);
     $('#publications').fadeOut(300);
     $('#posts-btn').removeClass('disabled');
-    $('#projects-btn').removeClass('disabled');
-    $('#publications-btn').removeClass('disabled')
-};
-var show_projects = function() {
-    $('#posts-btn').removeClass('disabled');
+    //$('#projects-btn').removeClass('disabled');
     $('#publications-btn').removeClass('disabled');
-    $('#publications').fadeOut(300);
-    $('#posts').fadeOut(300, function() {
-        $('#projects').fadeIn(300)
-    });
-    $('#projects-btn').addClass('disabled')
+    $('#drawings-btn').removeClass('disabled');
 };
+// var show_projects = function() {
+//     $('#posts-btn').removeClass('disabled');
+//     $('#publications-btn').removeClass('disabled');
+//     $('#publications').fadeOut(300);
+//     $('#posts').fadeOut(300, function() {
+//         $('#projects').fadeIn(300)
+//     });
+//     $('#projects-btn').addClass('disabled')
+// };
 var show_posts = function() {
-    $('#projects-btn').removeClass('disabled');
+    $('#drawings-btn').removeClass('disabled');
     $('#publications-btn').removeClass('disabled');
     $('#publications').fadeOut(300);
-    $('#projects').fadeOut(function() {
+    $('#drawings').fadeOut(function() {
         $('#posts').fadeIn(300)
     });
     $('#posts-btn').addClass('disabled')
 };
 var show_publications = function() {
-    $('#projects-btn').removeClass('disabled');
+    $('#drawings-btn').removeClass('disabled');
     $('#posts-btn').removeClass('disabled');
     $('#posts').fadeOut(300);
-    $('#projects').fadeOut(function() {
+    $('#drawings').fadeOut(function() {
         $('#publications').fadeIn(300)
     });
     $('#publications-btn').addClass('disabled')
+};
+var show_drawings = function() {
+    $('#publications-btn').removeClass('disabled');
+    $('#posts-btn').removeClass('disabled');
+    $('#posts').fadeOut(300);
+    $('#publications').fadeOut(function() {
+        $('#drawings').fadeIn(300)
+    });
+    $('#drawings-btn').addClass('disabled')
 };
